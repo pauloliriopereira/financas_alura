d934f87a92ff60a72d6875cc1e2ff876a229c640
pauloliriopereira <pauloliriopereira@gmail.com>
2018-07-24 13:47:26 -0300

    Atualização do projeto.

--- financas/src/main/java/br/com/alura/financas/teste/TesteConta.java
+++ financas/src/main/java/br/com/alura/financas/teste/TesteConta.java
@@ -1,10 +1,9 @@
 package br.com.alura.financas.teste;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import br.com.alura.financas.modelo.Conta;
+import br.com.alura.financas.util.JPAUtil;
 
 public class TesteConta 
 {
@@ -16,14 +15,10 @@ public class TesteConta
         conta.setAgencia( "123" );
         conta.setNumero( "456" );
         
-        EntityManagerFactory emf =  Persistence.createEntityManagerFactory( "financas" );
-        EntityManager em = emf.createEntityManager();
-        
+        EntityManager em = new JPAUtil().getEntityManager();
         em.getTransaction().begin();
-        em.persist(conta);
+        em.persist( conta );
         em.getTransaction().commit();
-
         em.close();
-        emf.close();
 	}
 }
